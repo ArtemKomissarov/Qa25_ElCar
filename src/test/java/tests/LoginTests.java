bec57b097819a3329f65368945e9bf0ad5867328
ArtemKomissarov <166131488+ArtemKomissarov@users.noreply.github.com>
2024-05-11 15:48:44 +0300

    login success

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,26 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
     @Test
-    public void test(){
+    public void loginSuccess(){
+
+
+       app.getHelperUser().openLoginForm();
+       app.getHelperUser().fillLoginForm("artem.komissarov.1999@gmail.com","Arte@Kom0607");
+       app.getHelperUser().submitLogin();
+
+       // Assert if element with text "Logget in success is present"
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+        app.getHelperUser().clickOKButton();
+
+
+
+
+
 
     }
 }
