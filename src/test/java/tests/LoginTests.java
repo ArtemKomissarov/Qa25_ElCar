66615aa4d006aa30375ad481c8f1e248eb5d4bdc
ArtemKomissarov <166131488+ArtemKomissarov@users.noreply.github.com>
2024-05-12 18:30:23 +0300

    negative login

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -53,6 +53,42 @@ public class LoginTests extends TestBase{
 
     }
 
+
+    @Test
+    public void loginWrongEmail(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("artem.komissarov.1999gmail.com","Arte@Kom0607");
+        app.getHelperUser().submitLogin();
+        Assert.assertEquals(app.getHelperUser().getErrorText(),"It'snot look like email");
+        Assert.assertTrue(app.getHelperUser().isYallaButtonNotActive());
+
+
+
+    }
+
+    @Test
+    public void loginWrongPassword(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("artem.komissarov.1999@gmail.com","Arte0607");
+        app.getHelperUser().submitLogin();
+
+
+    }
+
+
+    @Test
+    public  void loginUnregisteredUser(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("komisarov.1999@gmail.com","Ae@Kom0607");
+        app.getHelperUser().submitLogin();
+
+    }
+
+
+
+
+
+
     @AfterMethod
 
     public void postCondition(){
